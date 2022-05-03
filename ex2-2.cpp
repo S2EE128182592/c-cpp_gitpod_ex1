b2fb8d504eb2973310b9257fa0a3d9c8ab2f8197
S2EE128182592 <104436663+S2EE128182592@users.noreply.github.com>
2022-05-03 11:28:51 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World ");
+    
+    cout <<  "hello banana ";
+    cout <<  "my height is " << 1 << "m " << 50 << "cm";
+
+    return 0;
+}
